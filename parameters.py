c7ebd99b7e24bb8ee7b2436540c57724d167fe5e
Julio Balbino <julio4110@hotmail.com>
2023-10-17 17:07:03 -0300

    adjust parameters

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
-BASE_URL = "http://192.168.15.18:5000"
-#BASE_URL = "https://trolls.sa.ngrok.io/"
+#BASE_URL = "http://192.168.15.18:5000"
+BASE_URL = "https://trolls.sa.ngrok.io/"
 DIRECTORY = "data"
 UDP_PORT = 5053
-ARDUINO_PORT = "COM24"
+ARDUINO_PORT = "COM5"
 ARDUINO_SPEED = 9600
 BLOCK_USERS = True
 
